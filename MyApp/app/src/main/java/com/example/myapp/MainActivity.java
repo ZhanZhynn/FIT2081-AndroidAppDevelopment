465df8810d6692be67e0f86e2b75f3a6793d9953
Zz <zhanzhynn@gmail.com>
2023-03-06 16:45:00 +0800

    add toast

--- MyApp/app/src/main/java/com/example/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view){
+        Toast myToast = Toast.makeText(this, "Successfully added", Toast.LENGTH_SHORT);
+        myToast.show();
     }
 }
