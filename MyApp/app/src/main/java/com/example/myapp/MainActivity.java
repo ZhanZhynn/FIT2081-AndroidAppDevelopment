8cf59376baa6677647ec1b612adaffc4f502d435
Zz <zhanzhynn@gmail.com>
2023-03-06 17:28:52 +0800

    added clear input function

--- MyApp/app/src/main/java/com/example/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -4,18 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView bookId, bookTitle, bookISBN, bookAuthor, bookDescription, bookPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bookId = findViewById(R.id.bookID);
+        bookTitle = findViewById(R.id.bookTitle);
+        bookISBN = findViewById(R.id.bookISBN);
+        bookAuthor = findViewById(R.id.bookAuthor);
+        bookDescription = findViewById(R.id.bookDescription);
+        bookPrice = findViewById(R.id.bookPrice);
     }
 
     public void showToast(View view){
-        Toast myToast = Toast.makeText(this, "Successfully added", Toast.LENGTH_SHORT);
+        String toastString = "Successfully added '" + bookTitle.getText().toString() + " (RM" + bookPrice.getText().toString() + ")'.";
+        Toast myToast = Toast.makeText(this, toastString, Toast.LENGTH_LONG);
         myToast.show();
+        clearInput(view);
+    }
+
+    public void clearInput(View view){
+        //clear the input field
+        bookId.setText("");
+        bookTitle.setText("");
+        bookISBN.setText("");
+        bookAuthor.setText("");
+        bookDescription.setText("");
+        bookPrice.setText("");
+
     }
 }
