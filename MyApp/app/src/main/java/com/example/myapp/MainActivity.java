67ae56bbde283190e78e2b67b1a0558e3ba13216
Zz <zhanzhynn@gmail.com>
2023-03-15 17:02:32 +0800

    completed week 3 tasks

--- MyApp/app/src/main/java/com/example/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -32,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
         Toast myToast = Toast.makeText(this, toastString, Toast.LENGTH_LONG);
         myToast.show();
 //        clearInput(view);
+
+        //save last inserted book
+        SharedPreferences sharedPreferences = getSharedPreferences("book", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("bookId", bookId.getText().toString());
+        editor.putString("bookTitle", bookTitle.getText().toString());
+        editor.putString("bookISBN", bookISBN.getText().toString());
+        editor.putString("bookAuthor", bookAuthor.getText().toString());
+        editor.putString("bookDescription", bookDescription.getText().toString());
+        editor.putString("bookPrice", bookPrice.getText().toString());
+        editor.commit();
     }
 
     public void clearInput(View view){
@@ -50,5 +63,63 @@ public class MainActivity extends AppCompatActivity {
         bookPriceDbl = bookPriceDbl * 2;
         String bookPriceStr = String.format("%.2f", bookPriceDbl);
         bookPrice.setText(bookPriceStr);
+    }
+
+    //W3 Task 1: The activity must save and restore the view data of the book title and ISBN when you change its orientation.
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        //save only the view data of the book title and ISBN
+//        super.onSaveInstanceState(outState);
+        outState.putString("bookTitle", bookTitle.getText().toString());
+        outState.putString("bookISBN", bookISBN.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+        //restore on the view data of the book title and ISBN
+        bookTitle.setText(savedInstanceState.getString("bookTitle"));
+        bookISBN.setText(savedInstanceState.getString("bookISBN"));
+    }
+
+    //Task 2: The app must remember the last book added to the system. In other words, opening the app should load the attributes of the previous book added.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //save the last book added to the system
+        SharedPreferences sharedPreferences = getSharedPreferences("book", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("bookId", bookId.getText().toString());
+        editor.putString("bookTitle", bookTitle.getText().toString());
+        editor.putString("bookISBN", bookISBN.getText().toString());
+        editor.putString("bookAuthor", bookAuthor.getText().toString());
+        editor.putString("bookDescription", bookDescription.getText().toString());
+        editor.putString("bookPrice", bookPrice.getText().toString());
+        editor.commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //load the last book added to the system
+        SharedPreferences sharedPreferences = getSharedPreferences("book", MODE_PRIVATE);
+        bookId.setText(sharedPreferences.getString("bookId", ""));
+        bookTitle.setText(sharedPreferences.getString("bookTitle", ""));
+        bookISBN.setText(sharedPreferences.getString("bookISBN", ""));
+        bookAuthor.setText(sharedPreferences.getString("bookAuthor", ""));
+        bookDescription.setText(sharedPreferences.getString("bookDescription", ""));
+        bookPrice.setText(sharedPreferences.getString("bookPrice", ""));
+    }
+
+    //Task 3: Add a new button that is responsible for reloading the save attributes.
+    public void reload(View view){
+        //load the last book added to the system
+        SharedPreferences sharedPreferences = getSharedPreferences("book", MODE_PRIVATE);
+        bookId.setText(sharedPreferences.getString("bookId", ""));
+        bookTitle.setText(sharedPreferences.getString("bookTitle", ""));
+        bookISBN.setText(sharedPreferences.getString("bookISBN", ""));
+        bookAuthor.setText(sharedPreferences.getString("bookAuthor", ""));
+        bookDescription.setText(sharedPreferences.getString("bookDescription", ""));
+        bookPrice.setText(sharedPreferences.getString("bookPrice", ""));
     }
 }
